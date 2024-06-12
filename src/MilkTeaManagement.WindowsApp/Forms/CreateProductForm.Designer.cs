c25bb89e22a5ad9c3e7eba8b621e29ce6f670b96
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2024-06-12 13:28:41 +0700

    save changes

--- src/MilkTeaManagement.WindowsApp/Forms/CreateProductForm.Designer.cs
+++ src/MilkTeaManagement.WindowsApp/Forms/CreateProductForm.Designer.cs
@@ -29,12 +29,19 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            closeButton = new PictureBox();
             flowLayoutPanel1 = new FlowLayoutPanel();
             close = new PictureBox();
-            closeButton = new PictureBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            SearchTextBox = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)close).BeginInit();
             ((System.ComponentModel.ISupportInitialize)closeButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)close).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -48,6 +55,19 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(1024, 30);
             panel1.TabIndex = 1;
+            // 
+            // closeButton
+            // 
+            closeButton.BackColor = Color.Transparent;
+            closeButton.BackgroundImage = Properties.Resources.multiply;
+            closeButton.BackgroundImageLayout = ImageLayout.Stretch;
+            closeButton.Location = new Point(994, 0);
+            closeButton.Margin = new Padding(0);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new Size(30, 30);
+            closeButton.TabIndex = 13;
+            closeButton.TabStop = false;
+            closeButton.Click += closeButton_Click;
             // 
             // flowLayoutPanel1
             // 
@@ -69,34 +89,96 @@
             close.TabIndex = 12;
             close.TabStop = false;
             // 
-            // closeButton
+            // label1
             // 
-            closeButton.BackColor = Color.Transparent;
-            closeButton.BackgroundImage = Properties.Resources.multiply;
-            closeButton.BackgroundImageLayout = ImageLayout.Stretch;
-            closeButton.Location = new Point(994, 0);
-            closeButton.Margin = new Padding(0);
-            closeButton.Name = "closeButton";
-            closeButton.Size = new Size(30, 30);
-            closeButton.TabIndex = 13;
-            closeButton.TabStop = false;
-            closeButton.Click += closeButton_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 11F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(47, 104);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 39);
+            label1.TabIndex = 2;
+            label1.Text = "Poster:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(47, 156);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(319, 437);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Poppins", 11F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(430, 104);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 39);
+            label2.TabIndex = 4;
+            label2.Text = "Name:";
+            // 
+            // SearchTextBox
+            // 
+            SearchTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            SearchTextBox.BorderStyle = BorderStyle.FixedSingle;
+            SearchTextBox.Font = new Font("Poppins", 11F);
+            SearchTextBox.Location = new Point(430, 156);
+            SearchTextBox.Margin = new Padding(0);
+            SearchTextBox.Name = "SearchTextBox";
+            SearchTextBox.PlaceholderText = "   Product name";
+            SearchTextBox.Size = new Size(533, 40);
+            SearchTextBox.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Poppins", 11F);
+            textBox1.Location = new Point(430, 267);
+            textBox1.Margin = new Padding(0);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "   Product name";
+            textBox1.Size = new Size(533, 40);
+            textBox1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Poppins", 11F);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(430, 215);
+            label3.Margin = new Padding(0);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 39);
+            label3.TabIndex = 13;
+            label3.Text = "Name:";
             // 
             // CreateProductForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.FromArgb(64, 64, 64);
             ClientSize = new Size(1024, 640);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(SearchTextBox);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "CreateProductForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "CreateProductForm";
             panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)close).EndInit();
             ((System.ComponentModel.ISupportInitialize)closeButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)close).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -105,5 +187,11 @@
         private FlowLayoutPanel flowLayoutPanel1;
         private PictureBox close;
         private PictureBox closeButton;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private TextBox SearchTextBox;
+        private TextBox textBox1;
+        private Label label3;
     }
 }
